d5166e6a8522cb5a2e50c11427439fb0b0a53ac3
johnkenny54 <45182853+johnkenny54@users.noreply.github.com>
2024-09-18 11:52:08 -0700

    Normalize namespaces and clean up exception handling. (#4)
    * Normalize namespaces so SVG is always in default namespace.
    * Force all xlink namespace prefixes to "xlink:".
    * Check for non-SVG default namespaces; clean up exception handling.

---
+++ test/lib/parser-ns.test.js
@@ -0,0 +1,64 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { parseSvg } from '../../lib/parser.js';
+import { stringifySvg } from '../../lib/stringifier.js';
+import { SVGOError } from '../../lib/svgo/tools.js';
+
+const testFileDir = path.resolve(
+  path.dirname(fileURLToPath(import.meta.url)),
+  'namespaceData',
+);
+
+/**
+ * @param {string} filename
+ */
+function getData(filename) {
+  const input = fs.readFileSync(path.resolve(testFileDir, `${filename}.svg`), {
+    encoding: 'utf8',
+  });
+  const expected = fs.readFileSync(
+    path.resolve(testFileDir, `${filename}.expected.svg`),
+    {
+      encoding: 'utf8',
+    },
+  );
+  let actual = input;
+  try {
+    const parsed = parseSvg(input);
+    actual = stringifySvg(parsed, { pretty: true });
+  } catch (error) {
+    if (!(error instanceof SVGOError)) {
+      throw error;
+    }
+  }
+  return {
+    actual: actual,
+    expected: expected,
+  };
+}
+
+test('explicit svg namespace with no default', () => {
+  const data = getData('no-default-ns');
+  expect(data.actual).toBe(data.expected);
+});
+
+test('all non-xlink prefixes changed to xlink', () => {
+  const data = getData('xlink.1');
+  expect(data.actual).toBe(data.expected);
+});
+
+test('add xlink ns to top element not there', () => {
+  const data = getData('xlink.2');
+  expect(data.actual).toBe(data.expected);
+});
+
+test('add xlink ns to top element if already there', () => {
+  const data = getData('xlink.3');
+  expect(data.actual).toBe(data.expected);
+});
+
+test('fail if a default namespace other than SVG is declared - file should be unchanged', () => {
+  const data = getData('xlink.4');
+  expect(data.actual).toBe(data.expected);
+});

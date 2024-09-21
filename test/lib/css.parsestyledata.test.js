ce8512bc16c6dd6516dbbc1e398be95b71bac753
John Kenny <johnkenny54@yahoo.com>
2024-09-21 08:14:01 -0700

    Fix test for complex styles.

--- test/lib/css.parsestyledata.test.js
+++ test/lib/css.parsestyledata.test.js
@@ -1,4 +1,32 @@
-import { parseStyleDeclarations } from '../../lib/css.js';
+import { _isStyleComplex, parseStyleDeclarations } from '../../lib/css.js';
+
+describe('test whether a style attribute has complex declarations', function () {
+  const tests = [
+    {
+      input: 'fill:red;stroke:green',
+      expected: false,
+    },
+    {
+      input: 'fill:url(#a)',
+      expected: false,
+    },
+    {
+      input: 'mask: url(masks.svg#star) stroke-box;',
+      expected: true,
+    },
+    {
+      input: 'fill:rgb(96.862745%,93.72549%,71.764706%);',
+      expected: false,
+    },
+  ];
+
+  for (let index = 0; index < tests.length; index++) {
+    const test = tests[index];
+    it(`test ${test.input}`, function () {
+      expect(_isStyleComplex(test.input)).toBe(test.expected);
+    });
+  }
+});
 
 describe('test parsing of style attributes', function () {
   const tests = [

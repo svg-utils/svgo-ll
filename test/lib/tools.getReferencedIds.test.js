015e68d661e62260e01e834a8b00e4cfba7c29c7
John Kenny <johnkenny54@yahoo.com>
2024-09-24 07:12:15 -0700

    Added getReferencedIds() function.

---
+++ test/lib/tools.getReferencedIds.test.js
@@ -0,0 +1,187 @@
+import { getReferencedIds } from '../../lib/svgo/tools.js';
+
+/**
+ * @typedef {{
+ * elName:string,
+ * atts:[string,string][],
+ * expected:{id:string,attName:string}[]
+ * }} TestInfo
+ */
+
+/**
+ *
+ * @param {TestInfo} test
+ * @returns {import('../../lib/types.js').XastElement}
+ */
+function createElement(test) {
+  /** @type {import('../../lib/types.js').XastRoot} */
+  const root = { type: 'root', children: [] };
+  /** @type {import('../../lib/types.js').XastElement} */
+  const element = {
+    type: 'element',
+    name: test.elName,
+    parentNode: root,
+    attributes: {},
+    children: [],
+  };
+  for (const att of test.atts) {
+    element.attributes[att[0]] = att[1];
+  }
+  root.children.push(element);
+  return element;
+}
+
+describe('getReferencedIds()', () => {
+  /**
+   * @type {TestInfo[]}
+   */
+  const testData = [
+    {
+      elName: 'path',
+      atts: [],
+      expected: [],
+    },
+    {
+      elName: 'path',
+      atts: [['href', '#a']],
+      expected: [{ id: 'a', attName: 'href' }],
+    },
+    // url encoded
+    {
+      elName: 'path',
+      atts: [['href', '#%61']],
+      expected: [{ id: 'a', attName: 'href' }],
+    },
+    // non-ASCII
+    {
+      elName: 'path',
+      atts: [['href', '#康']],
+      expected: [{ id: '康', attName: 'href' }],
+    },
+    // non-ASCII encoded
+    {
+      elName: 'path',
+      atts: [['href', '#%E5%BA%B7']],
+      expected: [{ id: '康', attName: 'href' }],
+    },
+    // url() syntax doesn't work in href
+    {
+      elName: 'path',
+      atts: [['href', 'url(#a)']],
+      expected: [],
+    },
+    {
+      elName: 'path',
+      atts: [['href', '  #xY1-56']],
+      expected: [{ id: 'xY1-56', attName: 'href' }],
+    },
+    {
+      elName: 'path',
+      atts: [['xlink:href', '  #xY1-56   ']],
+      expected: [{ id: 'xY1-56', attName: 'xlink:href' }],
+    },
+    {
+      elName: 'path',
+      atts: [['fill', 'url(#ccc)']],
+      expected: [{ id: 'ccc', attName: 'fill' }],
+    },
+    // url encoded
+    {
+      elName: 'path',
+      atts: [['fill', 'url(#c%63c)']],
+      expected: [{ id: 'ccc', attName: 'fill' }],
+    },
+    // space between url and "(" non allowed
+    {
+      elName: 'path',
+      atts: [['fill', 'url (#ccc)']],
+      expected: [],
+    },
+    // non-ASCII
+    {
+      elName: 'path',
+      atts: [['fill', 'url(#康)']],
+      expected: [{ id: '康', attName: 'fill' }],
+    },
+    {
+      elName: 'path',
+      atts: [['fill', '  url(#ccc)']],
+      expected: [{ id: 'ccc', attName: 'fill' }],
+    },
+    {
+      elName: 'path',
+      atts: [['fill', '  url(  #ccc)']],
+      expected: [{ id: 'ccc', attName: 'fill' }],
+    },
+    {
+      elName: 'path',
+      atts: [['fill', '  url(  #ccc   )']],
+      expected: [{ id: 'ccc', attName: 'fill' }],
+    },
+    {
+      elName: 'path',
+      atts: [['fill', '  url(  #ccc   )  ']],
+      expected: [{ id: 'ccc', attName: 'fill' }],
+    },
+    {
+      elName: 'path',
+      atts: [['fill', 'url("#ccc")']],
+      expected: [{ id: 'ccc', attName: 'fill' }],
+    },
+    {
+      elName: 'path',
+      atts: [['fill', '  url("#ccc")']],
+      expected: [{ id: 'ccc', attName: 'fill' }],
+    },
+    {
+      elName: 'path',
+      atts: [['fill', '  url(  "#ccc")']],
+      expected: [{ id: 'ccc', attName: 'fill' }],
+    },
+    {
+      elName: 'path',
+      atts: [['fill', '  url(  "#ccc"   )']],
+      expected: [{ id: 'ccc', attName: 'fill' }],
+    },
+    {
+      elName: 'path',
+      atts: [['fill', '  url(  "#ccc"   )  ']],
+      expected: [{ id: 'ccc', attName: 'fill' }],
+    },
+    // Mismatched quotes.
+    {
+      elName: 'path',
+      atts: [['fill', 'url("#ccc\')']],
+      expected: [],
+    },
+    // Single quotes.
+    {
+      elName: 'path',
+      atts: [['fill', "url('#ccc')"]],
+      expected: [{ id: 'ccc', attName: 'fill' }],
+    },
+    // Style attribute
+    {
+      elName: 'path',
+      atts: [['style', 'fill:url(#ccc)']],
+      expected: [{ id: 'ccc', attName: 'style' }],
+    },
+    // Multiple references.
+    {
+      elName: 'path',
+      atts: [['style', 'fill:url(#ccc);stroke: url( #bbb)']],
+      expected: [
+        { id: 'ccc', attName: 'style' },
+        { id: 'bbb', attName: 'style' },
+      ],
+    },
+  ];
+
+  for (let index = 0; index < testData.length; index++) {
+    const test = testData[index];
+    it(`test ${test.atts}`, function () {
+      const ids = getReferencedIds(createElement(test));
+      expect(ids).toEqual(test.expected);
+    });
+  }
+});

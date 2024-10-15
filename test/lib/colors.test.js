b13be8ab4bf1c098dc1a43bbc889905c4ba57f29
johnkenny54 <45182853+johnkenny54@users.noreply.github.com>
2024-10-15 07:31:38 -0700

    Round RGB % colors to hex. (#56)

--- test/lib/colors.test.js
+++ test/lib/colors.test.js
@@ -1,7 +1,7 @@
 import { ColorValue } from '../../lib/color.js';
 
 describe('test parsing and minifying', () => {
-  /** @type {{in:string,minified?:string}[]} */
+  /** @type {{in:string,minified?:string,str?:string}[]} */
   const testCases = [
     { in: '#ffffff', minified: '#fff' },
     { in: '#aABBcc', minified: '#abc' },
@@ -11,7 +11,11 @@ describe('test parsing and minifying', () => {
     { in: 'wHatEveR' },
     { in: 'rGb( 50   100 ,   150 )', minified: '#326496' },
     { in: 'rgb(203,0,254)', minified: '#cb00fe' },
-    { in: 'rgb( 49.5%, 33.49% ,22.5% )', minified: 'rgb(49.5%,33.49%,22.5%)' },
+    {
+      in: 'rgb( 49.5%, 33.49% ,22.5% )',
+      minified: 'rgb(49.5%,33.49%,22.5%)',
+      str: 'rgb(49.5%,33.49%,22.5%)',
+    },
     { in: 'rgb(165,42,42)', minified: 'brown' },
     { in: 'rgb( 50 100 150 /.1)', minified: 'rgb( 50 100 150 /.1)' },
     { in: 'rgb(100%,0%,0%)', minified: 'red' },
@@ -20,7 +24,7 @@ describe('test parsing and minifying', () => {
   for (const testCase of testCases) {
     it(`${testCase.in}`, () => {
       const attValue = ColorValue.getColorObj(testCase.in);
-      expect(attValue.toString()).toBe(testCase.in);
+      expect(attValue.toString()).toBe(testCase.str ?? testCase.in);
       const minified = attValue.getMinifiedValue();
       expect(minified.toString()).toBe(testCase.minified ?? testCase.in);
     });

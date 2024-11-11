dff0f3a5e67147914307d6b7c04d076c9a92153a
John Kenny <johnkenny54@yahoo.com>
2024-11-11 11:09:18 -0800

    Get rid of call to detachNodeFromParent().

--- plugins/removeEmptyContainers.js
+++ plugins/removeEmptyContainers.js
@@ -1,4 +1,8 @@
-import { getHrefId } from '../lib/svgo/tools.js';
+import {
+  addChildToDelete,
+  deleteChildren,
+  getHrefId,
+} from '../lib/svgo/tools.js';
 import { detachNodeFromParent } from '../lib/xast.js';
 
 export const name = 'removeEmptyContainers';
@@ -28,9 +32,7 @@ const removableEls = new Set([
  */
 export const fn = () => {
   const removedIds = new Set();
-  /**
-   * @type {Map<string, import('../lib/types.js').XastElement[]>}
-   */
+  /** @type {Map<string, import('../lib/types.js').XastElement[]>} */
   const usesById = new Map();
 
   return {
@@ -49,48 +51,57 @@ export const fn = () => {
           }
         }
       },
-      exit: (node, parentNode) => {
+      exit: (element, parentNode) => {
         // remove only empty non-svg containers
-        if (!removableEls.has(node.name) || node.children.length !== 0) {
+        if (!removableEls.has(element.name) || element.children.length !== 0) {
           return;
         }
         // empty patterns may contain reusable configuration
         if (
-          node.name === 'pattern' &&
-          Object.keys(node.attributes).length !== 0
+          element.name === 'pattern' &&
+          Object.keys(element.attributes).length !== 0
         ) {
           return;
         }
         // The <g> may not have content, but the filter may cause a rectangle
         // to be created and filled with pattern.
-        if (node.name === 'g' && node.attributes.filter != null) {
+        if (element.name === 'g' && element.attributes.filter != null) {
           return;
         }
         // empty <mask> hides masked element
-        if (node.name === 'mask' && node.attributes.id != null) {
+        if (element.name === 'mask' && element.attributes.id != null) {
           return;
         }
         if (parentNode.type === 'element' && parentNode.name === 'switch') {
           return;
         }
 
-        detachNodeFromParent(node, parentNode);
-        if (node.attributes.id) {
-          removedIds.add(node.attributes.id);
+        // TODO: Change the way this works so that parent removes empty children. We can't queue them for deletion in
+        // root exit; this is running in element exit so that nested empty elements are removed from bottom up, the nesting
+        // would be hard to detect in root exit.
+        detachNodeFromParent(element, parentNode);
+        if (element.attributes.id) {
+          removedIds.add(element.attributes.id);
         }
       },
     },
     root: {
       exit: () => {
         // Remove any <use> elements that referenced an empty container.
+
+        /** @type {Map<import('../lib/types.js').XastParent,Set<import('../lib/types.js').XastChild>>} */
+        const childrenToDelete = new Map();
+
         for (const id of removedIds) {
           const usingEls = usesById.get(id);
           if (usingEls) {
             for (const element of usingEls) {
-              detachNodeFromParent(element);
+              addChildToDelete(childrenToDelete, element);
             }
           }
         }
+
+        deleteChildren(childrenToDelete);
       },
     },
   };

f9d6a449059b700e9f25735c13819a0eb31b225e
John Kenny <johnkenny54@yahoo.com>
2024-11-13 18:45:55 -0800

    Minify font-size.

--- test/lib/length.test.js
+++ test/lib/length.test.js
@@ -6,13 +6,13 @@ describe('test parsing and minifying', () => {
     { in: '.12', minified: '.12' },
     { in: '.12px', minified: '.12' },
     { in: '0.90em', minified: '.9em' },
+    { in: 'xx', minified: 'xx' },
   ];
   for (const testCase of testCases) {
     it(`${testCase.in}`, () => {
       const attValue = LengthOrPctValue.getLengthOrPctObj(testCase.in);
-      expect(attValue.toString()).toBe(testCase.in);
       const minified = attValue.getMinifiedValue();
-      expect(minified.toString()).toBe(testCase.minified ?? testCase.in);
+      expect(minified.toString()).toBe(testCase.minified ?? testCase);
     });
   }
 });

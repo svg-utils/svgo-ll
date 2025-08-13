ebb25d6dfabe0042560a28b13b7d4cdb82f36f89
John Kenny <johnkenny54@yahoo.com>
2025-08-13 06:45:14 -0700

    chore: remove removeViewBox plugin

--- plugins/plugins-types.d.ts
+++ plugins/plugins-types.d.ts
@@ -131,7 +131,6 @@ export type BuiltinsWithOptionalParams = DefaultPlugins & {
   removeScripts: void;
   removeStyleElement: void;
   removeTitle: void;
-  removeViewBox: void;
   removeXMLNS: void;
   round: {
     coordDigits?: number;

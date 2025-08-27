48d108d6ef316db9f696ecc037c1bd975b395958
John Kenny <johnkenny54@yahoo.com>
2025-08-27 10:44:12 -0700

    fix: getNumberOfDecimalDigits for exponents

--- test/lib/tools.getNumberOfDecimalDigits.test.js
+++ test/lib/tools.getNumberOfDecimalDigits.test.js
@@ -17,6 +17,15 @@ describe('getNumberOfDecimalDigits()', () => {
     { input: '1.20', expected: 1 },
     { input: '1.00', expected: 0 },
     { input: '1.', expected: 0 },
+    { input: '-2e-6', expected: 6 },
+    { input: '2.1e-3', expected: 4 },
+    { input: '2.10e-3', expected: 4 },
+    { input: '10e-1', expected: 0 },
+    { input: '1.234e2', expected: 1 },
+    { input: '20.00e-5', expected: 4 },
+    { input: '20e-5', expected: 4 },
+    { input: '20.e-5', expected: 4 },
+    { input: '20.01e5', expected: 0 },
   ];
 
   for (let index = 0; index < testData.length; index++) {

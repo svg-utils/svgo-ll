8c400f20c9cd654ea3a7476410da8fdd3a3e8215
John Kenny <johnkenny54@yahoo.com>
2025-08-27 11:52:18 -0700

    chore: set up test case

---
+++ test/plugin-combos/plugins.test.js
@@ -0,0 +1,30 @@
+import * as fs from 'node:fs';
+import * as path from 'node:path/posix';
+import { optimize } from '../../lib/svgo.js';
+
+/** @type {{name:string,config:import('../../lib/svgo.js').Config}[]} */
+const tests = [{ name: 'minifyPathData-round', config: { enable: ['round'] } }];
+
+describe('plugins tests', function () {
+  for (const test of tests) {
+    it(test.name, function () {
+      const inputFilePath = path.join(
+        import.meta.dirname,
+        'input-files',
+        test.name + '.svg',
+      );
+      const outputFilePath = path.join(
+        import.meta.dirname,
+        'output-files',
+        test.name + '.svg',
+      );
+      const input = fs.readFileSync(inputFilePath, 'utf8');
+      const expected = fs.readFileSync(outputFilePath, 'utf8');
+      const result = optimize(input, {
+        ...test.config,
+        js2svg: { pretty: true, indent: 2 },
+      });
+      expect(result.data).toBe(expected);
+    });
+  }
+});

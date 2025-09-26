a185b0fdd278cce1abfab56c9f9dddee0f192875
John Kenny <johnkenny54@yahoo.com>
2025-09-26 15:58:41 -0700

    fix: allow stroke-dasharray='none'

--- test/lib/strokeDashArrayValue.test.js
+++ test/lib/strokeDashArrayValue.test.js
@@ -7,6 +7,7 @@ describe('test parsing and minifying', () => {
     { in: '  0.8   0.9%  ', out: '.8 .9%' },
     { in: '  0.8 ,  0.9%  ', out: '.8 .9%' },
     { in: '  0.8,0.9%  ', out: '.8 .9%' },
+    { in: 'none', out: 'none' },
   ];
   for (const testCase of testCases) {
     it(`${testCase.in}`, () => {

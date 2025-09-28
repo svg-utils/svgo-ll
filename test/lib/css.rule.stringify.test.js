94789bd0a1fd8405d691f884f114b802ea92748b
John Kenny <johnkenny54@yahoo.com>
2025-09-28 07:59:31 -0700

    chore: remove px from non-font properties

--- test/lib/css.rule.stringify.test.js
+++ test/lib/css.rule.stringify.test.js
@@ -20,6 +20,10 @@ describe('test stringifying of properties for <style>', function () {
       props: [['opacity', '12']],
       expected: 'opacity:12',
     },
+    {
+      props: [['stroke-width', '12']],
+      expected: 'stroke-width:12',
+    },
   ];
 
   const sequences = [

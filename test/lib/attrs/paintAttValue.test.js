275faf44544548d000244f2764ebce78fdaee1d0
John Kenny <johnkenny54@yahoo.com>
2025-10-11 06:24:12 -0700

    fix: handle url with fallback

--- test/lib/attrs/paintAttValue.test.js
+++ test/lib/attrs/paintAttValue.test.js
@@ -3,6 +3,7 @@ import { PaintAttValue } from '../../../lib/attrs/paintAttValue.js';
 describe('test getURL()', () => {
   const tests = [
     { in: 'url(#a)', exp: true },
+    { in: 'url(#a) blue', exp: true },
     { in: 'yellow', exp: false },
   ];
 
@@ -14,6 +15,31 @@ describe('test getURL()', () => {
       } else {
         expect(obj.getURL()).toBeUndefined();
       }
+    });
+  }
+});
+
+describe('test toString()', () => {
+  const tests = [{ in: 'url(#a)' }, { in: 'url(#a) blue' }, { in: 'yellow' }];
+
+  for (const test of tests) {
+    it(`${test.in}`, () => {
+      const obj = PaintAttValue.getObj(test.in);
+      expect(obj.toString()).toBe(test.in);
+    });
+  }
+});
+
+describe('test getMinifiedValue()', () => {
+  const tests = [
+    { in: 'url(#a) #f00', out: 'url(#a) red' },
+    { in: 'yellow', out: '#ff0' },
+  ];
+
+  for (const test of tests) {
+    it(`${test.in}`, () => {
+      const obj = PaintAttValue.getObj(test.in).getMinifiedValue();
+      expect(obj.toString()).toBe(test.out ?? test.in);
     });
   }
 });

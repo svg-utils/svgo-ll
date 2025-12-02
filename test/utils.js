1a5367598d6e3ac94bc939221eed8b00c67c15d9
John Kenny <johnkenny54@yahoo.com>
2025-12-02 11:20:41 -0800

    chore: create getBoundingBox() tests

--- test/utils.js
+++ test/utils.js
@@ -1,3 +1,20 @@
+import { SvgAttMap } from '../lib/ast/svgAttMap.js';
+import { parseAttr } from '../lib/attrs/parseAttr.js';
+import { createElement, createRoot } from '../lib/xast.js';
+
+/**
+ * @param {string} name
+ * @param {Object<string,string>} atts
+ */
+export function createTestElement(name, atts) {
+  const root = createRoot();
+  const attMap = new SvgAttMap();
+  for (const [k, v] of Object.entries(atts)) {
+    attMap.set(k, parseAttr('elem', k, v));
+  }
+  return createElement(root, name, '', undefined, attMap);
+}
+
 /**
  * @param {import('../lib/types.js').XastRoot} root
  * @returns {import('../lib/types.js').XastElement}

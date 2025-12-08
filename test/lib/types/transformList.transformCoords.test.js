80225c7990a6abde40162be898d288215655cc28
johnkenny54 <45182853+johnkenny54@users.noreply.github.com>
2025-12-07 18:18:12 -0800

    feat: remove default y coordinates from gradient (#176)

--- test/lib/types/transformList.transformCoords.test.js
+++ test/lib/types/transformList.transformCoords.test.js
@@ -1,8 +1,9 @@
-import { ExactNum } from '../../../lib/exactnum.js';
 import { TransformList } from '../../../lib/types/transformList.js';
+import { ExactNum } from '../../../lib/utils/exactNum.js';
 
 const tests = [
   { transform: 'translate(1 2)', in: { x: 3, y: 4 }, expect: { x: 4, y: 6 } },
+  { transform: 'scale(2 3)', in: { x: 3, y: 4 }, expect: { x: 6, y: 12 } },
   { transform: 'rotate(1)', in: { x: 3, y: 4 }, expect: undefined },
 ];
 
